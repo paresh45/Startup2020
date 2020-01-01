3aae107ea4c119a8c4c26fe60487fb747df6e934
Pritish <pritish.2rout@gmail.com>
2020-01-01 07:41:23 +0530

    project setup sucessfully

---
+++ StartUpBackend/src/main/java/paresh/jan/StartUpBackend/App.java
@@ -0,0 +1,13 @@
+package paresh.jan.StartUpBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
